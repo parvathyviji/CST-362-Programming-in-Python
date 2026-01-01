fbf0a63af6eb6feebc5a9ae358c048f1b4d761de
parvathyviji <athyparu2005@gmail.com>
2026-01-01 23:05:38 +0530

    Question-05-AreaCircle.py

--- Module 1/Sequential Problems/Question-05-AreaCircle.py
+++ Module 1/Sequential Problems/Question-05-AreaCircle.py
@@ -1,3 +1,7 @@
 r=float(input("Enter the radius of the circle :"))
 area=3.14*r*r
 print("The area of the circle is :",area)
+
+#OUTPUT
+Enter the radius of the circle :5
+The area of the circle is : 78.5
