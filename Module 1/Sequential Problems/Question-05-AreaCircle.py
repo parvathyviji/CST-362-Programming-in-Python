d09177378b200c153fff3b56323b579078ba0ac6
parvathyviji <athyparu2005@gmail.com>
2026-01-01 23:16:14 +0530

    Question-05-AreaCircle.py

--- Module 1/Sequential Problems/Question-05-AreaCircle.py
+++ Module 1/Sequential Problems/Question-05-AreaCircle.py
@@ -5,3 +5,10 @@ print("The area of the circle is :",area)
 #OUTPUT
 Enter the radius of the circle :5
 The area of the circle is : 78.5
+
+#OUTPUT
+Enter side a: 3
+Enter side b: 4
+Enter side c: 5
+Perimeter = 12.0
+Area = 6.0
