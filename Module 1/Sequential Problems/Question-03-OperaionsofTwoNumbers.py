e3f49335dbde4e31b814fec5eab25c51260f3f0b
parvathyviji <athyparu2005@gmail.com>
2026-01-01 22:53:06 +0530

    Question-03-OperaionsofTwoNumbers.py

--- Module 1/Sequential Problems/Question-03-OperaionsofTwoNumbers.py
+++ Module 1/Sequential Problems/Question-03-OperaionsofTwoNumbers.py
@@ -5,3 +5,12 @@ print("Difference is", a - b)
 print("Product is", a * b)
 print("Quotient is", a / b)
 print("Remainder is", a % b)
+
+#OUTPUT
+Enter first number: 14
+Enter second number: 3
+Sum is 17
+Difference is 11
+Product is 42
+Quotient is 4.666666666666667
+Remainder is 2
