af4636c8c9923606b0d0c6202e66dbe1482c36d6
parvathyviji <athyparu2005@gmail.com>
2026-01-01 23:15:22 +0530

    Create Question-06-TriangleAreaPerimeter.py
    Add Question-06:Find the area and perimeter of a tfriangle

---
+++ Module 1/Sequential Problems/Question-06-TriangleAreaPerimeter.py
@@ -0,0 +1,8 @@
+a = float(input("Enter side a: "))
+b = float(input("Enter side b: "))
+c = float(input("Enter side c: "))
+p = a + b + c
+s = p / 2
+a= (s * (s - a) * (s - b) * (s - c))**0.5
+print("Perimeter =", p)
+print("Area =", a)
