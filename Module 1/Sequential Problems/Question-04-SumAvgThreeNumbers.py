479445bfcdfeb3bcde37f8979df92ef29312faf8
parvathyviji <athyparu2005@gmail.com>
2026-01-01 22:57:59 +0530

    Question-04-SumAvgThreeNumbers.py

--- Module 1/Sequential Problems/Question-04-SumAvgThreeNumbers.py
+++ Module 1/Sequential Problems/Question-04-SumAvgThreeNumbers.py
@@ -5,3 +5,10 @@ total = a + b + c
 average = total / 3
 print("Sum =", total)
 print("Average =", average)
+
+#OUTPUT
+Enter first number: 4
+Enter second number: 3
+Enter third number: 1
+Sum = 8
+Average = 2.6666666666666665
