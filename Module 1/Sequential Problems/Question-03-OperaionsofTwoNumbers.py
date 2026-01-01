a98447b0118ce55a08564fa45a2b28a0d65c9630
parvathyviji <athyparu2005@gmail.com>
2026-01-01 22:51:42 +0530

    Create Question-03-OperaionsofTwoNumbers.py
    Add Question-03: perform the operations on two numbers

---
+++ Module 1/Sequential Problems/Question-03-OperaionsofTwoNumbers.py
@@ -0,0 +1,7 @@
+a=int(input("Enter first number: "))
+b=int(input("Enter second number: "))
+print("Sum is", a + b)
+print("Difference is", a - b)
+print("Product is", a * b)
+print("Quotient is", a / b)
+print("Remainder is", a % b)
