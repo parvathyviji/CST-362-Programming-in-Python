378514bda575e263fddefbaece1881272e774371
parvathyviji <athyparu2005@gmail.com>
2026-01-01 23:17:18 +0530

    Question-06-TriangleAreaPerimeter.py

--- Module 1/Sequential Problems/Question-06-TriangleAreaPerimeter.py
+++ Module 1/Sequential Problems/Question-06-TriangleAreaPerimeter.py
@@ -6,3 +6,10 @@ s = p / 2
 a= (s * (s - a) * (s - b) * (s - c))**0.5
 print("Perimeter =", p)
 print("Area =", a)
+
+#OUTPUT
+Enter side a: 3
+Enter side b: 4
+Enter side c: 5
+Perimeter = 12.0
+Area = 6.0
