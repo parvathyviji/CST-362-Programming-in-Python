42ec3127cd5964a0091f381b5a7bece57792a95b
parvathyviji <athyparu2005@gmail.com>
2026-01-01 22:56:56 +0530

    Create Question-04-SumAvgThreeNumbers.py
    Add Question-04: Sum and average of three numbers

---
+++ Module 1/Sequential Problems/Question-04-SumAvgThreeNumbers.py
@@ -0,0 +1,7 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c = int(input("Enter third number: "))
+total = a + b + c
+average = total / 3
+print("Sum =", total)
+print("Average =", average)
