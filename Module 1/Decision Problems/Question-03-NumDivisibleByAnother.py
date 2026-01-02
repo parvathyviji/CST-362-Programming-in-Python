c29fedfb8c8f8b60fb19fb6f74903698b92f6caf
parvathyviji <athyparu2005@gmail.com>
2026-01-02 20:35:54 +0530

    Create Question-03-NumDivisibleByAnother.py
    Add Question-03:check whether number is comppletely divisible or not

---
+++ Module 1/Decision Problems/Question-03-NumDivisibleByAnother.py
@@ -0,0 +1,6 @@
+a=int(input("Enter 1st number :"))
+b=int(input("Enter 2nd number :"))
+if a%b==0:
+    print("Number is completely divisible.")
+else:
+    print("Number is not completely divisible.")
